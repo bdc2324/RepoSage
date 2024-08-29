42739709d67c8b786655986212fe3e3a09de0c03
Brendan Carney <brendancarney24@gmail.com>
2024-08-29 16:39:23 -0400

    added basic app page with chat window

--- src/app.py
+++ src/app.py
@@ -1,1 +1,32 @@
-print('hello')
+# Libraries
+import os
+import toml
+
+import streamlit as st
+
+
+CONFIG_PATH = os.path.join(".streamlit", "config.toml")
+config = toml.load(CONFIG_PATH)
+
+def chat_interface():
+    st.text_area(label = "Paste Link Here")
+    st.button(label = "Submit")
+
+
+def main():
+    """
+    """
+    st.set_page_config(
+        page_title = config["PAGES"]["HOME"]["NAME"],
+        layout = "wide"
+    )
+    st.title(config["PAGES"]["HOME"]["TITLE"])
+    #st.write(config["PAGES"]["HOME"]["SUBTITLE"])
+
+    chat_interface()
+
+if __name__ == "__main__":
+    main()
+
+
+
